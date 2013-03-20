03d0306638e11ac8e415c61ccf93b21baf9609c0
Thorsten Lorenz <thlorenz@gmx.de>
2013-03-19 22:19:49 -0400

    working implementation that caches

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 'use strict';
 
-var coffeeify = require('coffeeify');
+var coffee    =  require('coffee-script') 
+  , convert   =  require('convert-source-map')
+  , through   =  require('through')
+  , crypto    =  require('crypto')
+  , cache     =  {};
+
+function getHash(data) {
+  crypto
+    .createHash('md5')
+    .update(data)
+    .digest('hex');
+}
+
+function compile(file, data) {
+  var compiled = coffee.compile(data, { sourceMap: true, generatedFile: file, inline: true })
+    , comment = convert
+        .fromJSON(compiled.v3SourceMap)
+        .setProperty('sources', [ file ]) 
+        .toComment();
+
+  return compiled.js + '\n' + comment;
+}
 
 module.exports = function (file) {
-  return coffeeify(file);
-};
+  if (!/\.coffee$/.test(file)) return through();
+    
+  var data = '';
+  return through(write, end);
+  
+  function write (buf) { data += buf; }
+  function end () {
 
-module.exports.__defineSetter__('root', function (root) { coffeeify.root = root; });
+    var hash = getHash(data)
+      , cached = cache[file];
+
+    if (!cached || cached.hash !== hash) {
+      cache[file] = { compiled: compile(file, data), hash: hash };
+    }   
+
+    this.queue(cache[file].compiled);
+    this.queue(null);
+  }
+};
