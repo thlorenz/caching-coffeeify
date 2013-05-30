c57be47225c30ae13e2379eb353dfb013816dc75
Johan Nordberg <its@johan-nordberg.com>
2013-05-30 17:04:45 +0200

    Emit compile errors on stream instead of throwing

--- index.js
+++ index.js
@@ -36,8 +36,13 @@ module.exports = function (file) {
       , cached = cache[file];
 
     if (!cached || cached.hash !== hash) {
-      cache[file] = { compiled: compile(file, data), hash: hash };
-    }   
+      try {
+        cache[file] = { compiled: compile(file, data), hash: hash };
+      } catch (error) {
+        this.emit('error', error);
+        return;
+      }
+    }
 
     this.queue(cache[file].compiled);
     this.queue(null);
